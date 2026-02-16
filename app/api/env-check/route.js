9a8eb261a0cf4dbd9bffe42b4495ea4ca6f5cfae
SueEdgerley <sue.edgerley@croydon.gov.uk>
2026-02-16 14:31:02 +0000

    Update route.js

--- app/api/env-check/route.js
+++ app/api/env-check/route.js
@@ -4,12 +4,5 @@ export async function GET() {
   return NextResponse.json({
     hasPublishable: !!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     hasSecret: !!process.env.CLERK_SECRET_KEY,
-    publishableKeyPrefix: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY 
-      ? process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY.substring(0, 10) + '...' 
-      : 'not set',
-    secretKeyPrefix: process.env.CLERK_SECRET_KEY 
-      ? process.env.CLERK_SECRET_KEY.substring(0, 10) + '...' 
-      : 'not set',
-    domain: process.env.NEXT_PUBLIC_CLERK_DOMAIN || 'not set (defaults to estateinspections.co.uk)',
   });
 }
