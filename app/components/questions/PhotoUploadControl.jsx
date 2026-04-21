3345b60ffa81a96df886116aa27ac8afd9c5e2b8
SueEdgerley <sue.edgerley@croydon.gov.uk>
2026-04-21 19:58:03 +0100

    nv form

--- app/components/questions/PhotoUploadControl.jsx
+++ app/components/questions/PhotoUploadControl.jsx
@@ -114,7 +114,7 @@ export default function PhotoUploadControl({
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', width: '100%', minWidth: 0, boxSizing: 'border-box' }}>
       <input
         ref={inputRef}
         {...(id ? { id } : {})}
